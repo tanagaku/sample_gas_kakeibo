b1b544b4ffd931d53ee50cf1aafa714037e24879
tanagaku <aegidousi@outlook.jp>
2022-05-28 15:23:05 +0900

    change:get properties from sheet

--- src/constant.ts
+++ src/constant.ts
@@ -1,8 +1,39 @@
-export const CATEGORY_LIST = ['食費', '外食費', '日用品', 'ヘルスケア', '娯楽費', '電気代', 'ガス代', '水道代', '家賃', '入金', 'その他']
+import * as line_fetch from './line_fetch';
+
+export const CATEGORY_LIST = getProperties(1)
 export const DAY_LIST = ['今日', '昨日', '一昨日']
-export const PAYMENT_STATUS_LIST = ['共通財布', '精算済', '未精算']
+export const PAYMENT_STATUS_LIST = getProperties(2)
 export const HELP_MESSAGE_LIST = ['ヘルプ', 'カテゴリ', '支払い状況']
 export const DELETE = '削除'
 export const SHEET_NAME = 'List'
 export const ACCOUNT_LIST = ['今月', '先月', '残高']
 export const HELP_MESSAGE = '入力は\n1行目:カテゴリ\n2行目:金額\n3行目:購入日\n4行目:支払い状況\nを入力してください。\n残高確認は\n' + ACCOUNT_LIST + ',指定したい年月日(yyyy/MM/dd)\nを入力してください。'
+
+
+//削除メッセージ受信時
+function getProperties(row: number) {
+
+  const categories: string[] = []
+
+  if (line_fetch.SHEET_ID == null) {
+    console.error('failed to get spreadsheet')
+    return categories
+  }
+
+  var sheets = SpreadsheetApp.openById(line_fetch.SHEET_ID).getSheetByName(SHEET_NAME);
+  if (sheets == null) {
+    console.error('failed to get spreadsheet')
+    return categories
+  }
+
+  const lastRow = sheets.getLastRow()
+
+  for (let i = 1; i < lastRow; i++) {
+    const record = sheets.getRange(i, row).getValue()
+    if (!record) {
+      return
+    }
+    categories.push(record)
+  }
+  return categories
+}
